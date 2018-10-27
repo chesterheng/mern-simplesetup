b0e0bb47f3fef4ad65123d49b800af36e685824c
Chester Heng <chester.heng@gmail.com>
2018-10-27 21:06:59 +0800

    First commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import path from 'path';
+import template from './../template';
+import { MongoClient } from 'mongodb';
+
+// development only
+import devBundle from './devBundle';
+
+const app = express();
+// development only
+devBundle.compile(app);
+
+const CURRENT_WORKING_DIR = process.cwd();
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+
+app.get('/', (req, res) => {
+  res.status(200).send(template());
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('Server started on port %s.', port);
+});
+
+const url =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup';
+MongoClient.connect(
+  url,
+  (err, db) => {
+    console.log('Connected successfully to mongodb server');
+    db.close();
+  }
+);
